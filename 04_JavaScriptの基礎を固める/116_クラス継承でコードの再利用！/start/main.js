7f3e132369143f90a9ea406ddcc030e3641054dc
h-sogawa <89685474+h-sogawa@users.noreply.github.com>
2022-04-20 10:20:37 +0900

    learn js class

--- 04_JavaScriptの基礎を固める/116_クラス継承でコードの再利用！/start/main.js
+++ 04_JavaScriptの基礎を固める/116_クラス継承でコードの再利用！/start/main.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const btn = document.querySelector('#btn');
-    const ta = new TextAnimation('.animate-title');
-    const ta2 = new TextAnimation2('.animate-title-2');
+    const ta = new TweenTextAnimation('.tween-animate-title');
     ta.animate();
-    ta2.animate();
     btn.addEventListener('click', ta.animate.bind(ta));
 });
 
 
 class TextAnimation {
     constructor(el) {
-        this.el = document.querySelector(el);
-        this.chars = this.el.innerHTML.trim().split("");
-        this.el.innerHTML = this._splitText();
+        this.DOM = {};
+        this.DOM.el = document.querySelector(el);
+        //  this.DOM.el = document.querySelector(el);
+        this.chars =  this.DOM.el.innerHTML.trim().split("");
+         this.DOM.el.innerHTML = this._splitText();
     }
     _splitText() {
         return this.chars.reduce((acc, curr) => {
@@ -21,18 +21,29 @@ class TextAnimation {
         }, "");
     }
     animate() {
-        this.el.classList.toggle('inview');
+         this.DOM.el.classList.toggle('inview');
         console.log('test');
     }
 }
 
 //sassみたいにextendsで継承できる
-class TextAnimation2 extends TextAnimation {
+class TweenTextAnimation extends TextAnimation {
   constructor(el) {
     super(el); //親のconstructorを継承する
+    this.DOM.el.chars = this.DOM.el.querySelectorAll('.char');
   }
 
   animate() { //同じ関数名だとオーバーライドされる
+    this.DOM.el.classList.add('inview');
+    this.DOM.el.chars.forEach((c, i) => {
+      TweenMax.to(c, .6, {
+        ease: Back.easeOut,
+        delay: i * .05,
+        startAt: {y: '-50%', opacity: 0},
+        y: '0%',
+        opacity: 1,
+      });
+    });
     console.log('asdfadsf');
   }
 }
